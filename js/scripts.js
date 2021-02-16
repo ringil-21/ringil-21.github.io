2adb53ecc20dd6144ba300b913c2a1fcbbe73d14
Alois Meier <alois.wever@icloud.com>
2021-02-16 01:56:01 +0100

    Dark mode support and datenschutz page

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+function switchDisplayMode(mode){
+    var element = document.getElementById("body");
+    
+
+    var newMode = mode
+    
+    /*If no mode is set, toogle*/
+    if(newMode  == null)
+    	newMode = element.className.indexOf("light") > -1 ? "dark" : "light";
+
+    
+    element.classList.remove("light");
+    element.classList.remove("dark");
+
+    
+    if(newMode == "light"){
+        element.classList.add("light");
+    }
+    else if(newMode == "dark"){
+        element.classList.add("dark");
+
+    }
+    else{
+        element.classList.add("light");
+
+    }
+}
+
+function checkInitDisplayMode(){
+console.log("CHECK")
+	var mode = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"   
+ 	switchDisplayMode(mode)
+}
