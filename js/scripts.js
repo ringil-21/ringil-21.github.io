31592fd2f79ddb90e309e6453f1105003baf18bc
Alois Meier <alois.wever@icloud.com>
2021-12-07 16:41:44 +0100

    New Logo

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,52 @@
-function switchDisplayMode(mode){
-    var element = document.getElementById("body");
-    
-
-    var newMode = mode
-    
-    /*If no mode is set, toogle*/
-    if(newMode  == null)
-    	newMode = element.className.indexOf("light") > -1 ? "dark" : "light";
-
-    
-    element.classList.remove("light");
-    element.classList.remove("dark");
-
-    
-    if(newMode == "light"){
-        element.classList.add("light");
-    }
-    else if(newMode == "dark"){
-        element.classList.add("dark");
-
-    }
-    else{
-        element.classList.add("light");
-
-    }
+function onBodyLoad(){
+    checkInitDisplayMode();
+    consoleMsg();
 }
 
-function checkInitDisplayMode(){
+function switchDisplayMode(mode) {
+  var element = document.getElementById("body");
+  var newMode = mode;
 
-	/*Check for darkmode status of browser*/
-	var mode = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
-	
-	/* If there is no darkmode support, switch to darkmode between 22:00 and 05:00*/
-	if(mode == "light"){
-		var date = new Date();
-		if(date.getHours() < 6 || date.getHours > 21)
-			mode = "dark"
-			
-	}
-	
- 	switchDisplayMode(mode)
+  /*If no mode is set, toogle*/
+  if (newMode == null)
+    newMode = element.className.indexOf("light") > -1 ? "dark" : "light";
+
+  element.classList.remove("light");
+  element.classList.remove("dark");
+
+  if (newMode == "light") {
+    element.classList.add("light");
+  } else if (newMode == "dark") {
+    element.classList.add("dark");
+  } else {
+    element.classList.add("light");
+  }
+}
+
+function checkInitDisplayMode() {
+  /*Check for darkmode status of browser*/
+  var mode = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+
+  /* If there is no darkmode support, switch to darkmode between 22:00 and 05:00*/
+  if (mode == "light") {
+    var date = new Date();
+    if (date.getHours() < 6 || date.getHours > 21) mode = "dark";
+  }
+  switchDisplayMode(mode);
+}
+
+function consoleMsg(){
+    let msg = "%c Hi 👋! Welcome to Ringil Projects!";
+    let styles = [
+      "font-size: 12px",
+      "font-family: monospace",
+      "background: white",
+      "display: inline-block",
+      "color: black",
+      "padding: 8px 19px",
+      "border: 1px dashed;",
+    ].join(";");
+    console.log(msg, styles);
 }
